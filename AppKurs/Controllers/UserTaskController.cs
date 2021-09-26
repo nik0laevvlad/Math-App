68d5aaaa96cdbb8c10a38f71a599db22c0c13533
Владислав Николаев <vladislavn340@gmail.com>
2021-09-26 14:57:37 +0300

    details cshtml

--- AppKurs/Controllers/UserTaskController.cs
+++ AppKurs/Controllers/UserTaskController.cs
@@ -1,6 +1,7 @@
 ﻿using AppKurs.Data;
 using AppKurs.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,10 +30,31 @@ namespace AppKurs.Controllers
         [HttpPost]
         public IActionResult Create([FromForm] UserTask model)
         {
-            UserTask uTask = new UserTask { TaskTitle = model.TaskTitle, TaskText = model.TaskText, TaskAnswer = model.TaskAnswer, TaskUser = User.Identity.Name, TaskTopic = model.TaskTopic };
+            UserTask uTask = new UserTask { TaskTitle = model.TaskTitle,
+                                            TaskText = model.TaskText,
+                                            TaskAnswer = model.TaskAnswer,
+                                            TaskUser = User.Identity.Name,
+                                            TaskTopic = model.TaskTopic };
             _db.UserTasks.Add(uTask);
             _db.SaveChanges();
             return View(model);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usertask = await _db.UserTasks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usertask == null)
+            {
+                return NotFound();
+            }
+
+            return View(usertask);
         }
     }
 }
