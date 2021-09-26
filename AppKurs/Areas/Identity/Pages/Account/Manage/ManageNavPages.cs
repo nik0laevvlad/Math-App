5fe154d8abafecb864cafc7c9db7376a1234bc1e
Владислав Николаев <vladislavn340@gmail.com>
2021-09-26 18:38:38 +0300

    fix account/manage

--- AppKurs/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ AppKurs/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using AppKurs.Data;
 
 namespace AppKurs.Areas.Identity.Pages.Account.Manage
 {
